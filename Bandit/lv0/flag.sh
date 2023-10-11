6a9d7e21f27c85e08c82dac6cb62b39c187edf31
occupytheroot <92579003+tantien-hmtt@users.noreply.github.com>
2023-10-11 23:37:08 +0700

    lv0

---
+++ Bandit/lv0/flag.sh
@@ -0,0 +1,4 @@
+ssh bandit0@bandit.labs.overthewire.org -p 2220 #ssh
+bandit0                                         #nhập password          
+ls                                              #xem các file có trong thư mục                                
+cat readme                                      #xem nội dụng trong file                   
